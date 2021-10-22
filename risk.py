0b1dd886cc721792e76cb25048599e2dcec0671b
lucs500 <lucasbezrocha@gmail.com>
2021-10-22 17:54:50 -0300

    Update risk.py

--- risk.py
+++ risk.py
@@ -7,7 +7,7 @@ import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 import yfinance as yf
 
-df = quandl.get("BCHAIN/MKPRU", api_key="veu3p-4jnmTcfDVF9SuH").reset_index()   #your quandl api key
+df = quandl.get("BCHAIN/MKPRU", api_key="YOUR_KEY").reset_index()   #your quandl api key
 
 btcdata = yf.download(tickers='BTC-USD', period="1d", interval="1m")["Close"]
 lastprice = btcdata.iloc[-1]
@@ -17,14 +17,16 @@ df = df[df["Value"] > 0]
 df["Date"] = pd.to_datetime(df["Date"])
 df.sort_values(by="Date", inplace=True)
 f_date = pd.to_datetime(date(2010, 1, 1))
-E_date = pd.to_datetime(date(2009, 1, 3))
+E_date = pd.to_datetime(date(2009, 1, 3))   #genesis
 delta = f_date - E_date
 df = df[df.Date > f_date]
 df.reset_index(inplace=True)
 
+
 def normalization(data):
     normalized = (data - data.min()) / (data.max() - data.min())
     return normalized
+
 
 def ossValue(days):
     X = np.array(np.log10(df.ind[:days])).reshape(-1, 1)
@@ -32,6 +34,7 @@ def ossValue(days):
     reg = LinearRegression().fit(X, y)
     values = reg.predict(X)
     return values[-1]
+
 
 def normalizationhalving(Normlist):
     global df
@@ -47,37 +50,37 @@ def normalizationhalving(Normlist):
     df = pd.concat([df1, df2, df3, df4])
     return df
 
-df["Mayer"] = df["Value"] / df["Value"].rolling(200).mean()
+df["Mayer"] = df["Value"] / df["Value"].rolling(200).mean()   #mayer multiple
 
 df["btcIssuance"] = 7200 / 2 ** (np.floor(df["index"] / 1458))
 df["usdIssuance"] = df["btcIssuance"] * df["Value"]
 df["MAusdIssuance"] = df["usdIssuance"].rolling(window=365).mean()
-df["usdoverMA"] = df["usdIssuance"] / df["MAusdIssuance"]
+df["PuellMultiple"] = df["usdIssuance"] / df["MAusdIssuance"]   #puell multiple
 
-df["50d/50w"] = df["Value"] / df["Value"].ewm(span=365).mean()
+df["Price/52w"] = df["Value"] / df["Value"].ewm(span=365).mean()   #price/52w ma
 
 df["Return%"] = df["Value"].pct_change() * 100
-df["Sharpe"] = (df["Return%"].rolling(365).mean() - 1) / df["Return%"].rolling(365).std()
+df["Sharpe"] = (df["Return%"].rolling(365).mean() - 1) / df["Return%"].rolling(365).std()   #sharpe
 
 dfs = df[df["Return%"] < 0]
-df["Sortino"] = (df["Return%"].rolling(365).mean() - 1) / dfs["Return%"].rolling(365).std()
+df["Sortino"] = (df["Return%"].rolling(365).mean() - 1) / dfs["Return%"].rolling(365).std()   #sortino
 
 df["ind"] = [x + delta.days for x in range(len(df))]
-df["ossvalues"] = np.log10(df.Value) - [ossValue(x + 1) for x in range(len(df))]
+df["PowerLaw"] = np.log10(df.Value) - [ossValue(x + 1) for x in range(len(df))]   #power law
 
-df.update(normalizationhalving(["usdoverMA", "50d/50w", "ossvalues", "Sharpe", "Sortino", "Mayer"]))
-df["avg"] = df[["usdoverMA", "50d/50w", "ossvalues", "Sharpe", "Mayer"]].mean(axis=1)
+df.update(normalizationhalving(["PuellMultiple", "Price/52w", "PowerLaw", "Sharpe", "Sortino","Mayer"]))
+df["avg"] = df[["PuellMultiple","Price/52w", "PowerLaw","Sharpe"]].mean(axis=1)   #avg
 
 fig = make_subplots(specs=[[{"secondary_y": True}]])
 xaxis = df.Date
 
-#fig.add_trace(go.Scatter(x=xaxis, y=df["usdoverMA"], name="Puell Multiple", line=dict(color="white")), secondary_y=True)   #puell multiple
-#fig.add_trace(go.Scatter(x=xaxis, y=df["50d/50w"], name="Risk_MA", line=dict(color="white")), secondary_y=True)   #50d/50w ma
-#fig.add_trace(go.Scatter(x=xaxis, y=df["ossvalues"], name="power law", line=dict(color="white")), secondary_y=True)   #power law
-#fig.add_trace(go.Scatter(x=xaxis, y=df["Sharpe"], name="Sharpe", line=dict(color="blue")), secondary_y=True)   #sharpe ratio
-#fig.add_trace(go.Scatter(x=xaxis, y=df["Sortino"], name="Sortino", line=dict(color="pink")), secondary_y=True)   #sortino ratio
-#fig.add_trace(go.Scatter(x=xaxis, y=df["Mayer"], name="Mayer", mode="lines", line=dict(color="white")), secondary_y=True)   #mayer multiple
 fig.add_trace(go.Scatter(x=xaxis, y=df.Value, name="Price", line=dict(color="gold")), secondary_y=False)
+#fig.add_trace(go.Scatter(x=xaxis, y=df["PuellMultiple"], name="Puell Multiple", line=dict(color="white")), secondary_y=True)   #puell multiple
+#fig.add_trace(go.Scatter(x=xaxis, y=df["Price/52w"], name="Risk_MA", line=dict(color="white")), secondary_y=True)   #price/52w ma
+#fig.add_trace(go.Scatter(x=xaxis, y=df["PowerLaw"], name="power law", line=dict(color="white")), secondary_y=True)   #power law
+#fig.add_trace(go.Scatter(x=xaxis, y=df["Sharpe"], name="Sharpe", line=dict(color="white")), secondary_y=True)   #sharpe ratio
+#fig.add_trace(go.Scatter(x=xaxis, y=df["Sortino"], name="Sortino", line=dict(color="white")), secondary_y=True)   #sortino ratio
+#fig.add_trace(go.Scatter(x=xaxis, y=df["Mayer"], name="Mayer", mode="lines", line=dict(color="white")), secondary_y=True)   #mayer multiple
 fig.add_trace(go.Scatter(x=xaxis, y=df["avg"], name="Risk", mode="lines", line=dict(color="white")), secondary_y=True)
 
 fig.add_hrect(y0=0.2, y1=0, line_width=0, fillcolor="green", opacity=0.5, secondary_y=True)
@@ -89,24 +92,13 @@ fig.add_hrect(y0=0.7, y1=0.8, line_width=0, fillcolor="red", opacity=0.4, second
 fig.add_hrect(y0=0.8, y1=0.9, line_width=0, fillcolor="red", opacity=0.5, secondary_y=True)
 fig.add_hrect(y0=0.9, y1=1.0, line_width=0, fillcolor="red", opacity=0.6, secondary_y=True)
 
-fig.update_layout(xaxis_title='Date', yaxis_title='Price',
-                  yaxis2_title='Risk',
-                  yaxis1=dict(type='log', showgrid=False),
-                  yaxis2=dict(showgrid=True, tickmode='linear', tick0=0.0, dtick=0.1),
-                  template="plotly_dark")
-fig.show()
-
-fig = make_subplots(specs=[[{"secondary_y": True}]])
-xaxis = df.Date
-
-fig.add_trace(go.Scatter(x=xaxis, y=df["usdoverMA"], name="Puell Multiple", line=dict(color="blue")), secondary_y=True)   #puell multiple
-fig.add_trace(go.Scatter(x=xaxis, y=df["50d/50w"], name="Risk_MA", line=dict(color="pink")), secondary_y=True)   #50d/50w ma
-fig.add_trace(go.Scatter(x=xaxis, y=df["Mayer"], name="Mayer", mode="lines", line=dict(color="white")), secondary_y=True)   #mayer multiple
-fig.add_trace(go.Scatter(x=xaxis, y=df.Value, name="Price", line=dict(color="gold")), secondary_y=False)
+AnnotationText = f"BTC price: {round(df['Value'].iloc[-1])} - Risk: {round(df['avg'].iloc[-1],2)}"
 
 fig.update_layout(xaxis_title='Date', yaxis_title='Price',
                   yaxis2_title='Risk',
                   yaxis1=dict(type='log', showgrid=False),
                   yaxis2=dict(showgrid=True, tickmode='linear', tick0=0.0, dtick=0.1),
-                  template="plotly_dark")
+                  template="plotly_dark",
+                  title={'text': AnnotationText, 'y': 0.9, 'x': 0.5})
+
 fig.show()
